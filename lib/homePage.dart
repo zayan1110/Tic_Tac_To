82ebdad0ff8f24c3de8ca2b965b5e6df963261e8
zayan1110 <mohamedzayanali990@gmail.com>
2024-05-29 16:23:08 +0300

    project tic tac to

---
+++ lib/homePage.dart
@@ -0,0 +1,257 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool ohTurn = true;
+  List<String> displayExOh = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ];
+
+  var myTextStyle = TextStyle(color: Colors.white, fontSize: 30);
+  int exScore = 0;
+  int ohSecore = 0;
+  int filledBoxes = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: Column(
+        children: [
+          Expanded(
+              child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Player x',
+                        style: myTextStyle,
+                      ),
+                      Text(
+                        exScore.toString(),
+                        style: myTextStyle,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Player o',
+                        style: myTextStyle,
+                      ),
+                      Text(
+                        ohSecore.toString(),
+                        style: myTextStyle,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )),
+          Expanded(
+            flex: 3,
+            child: GridView.builder(
+              itemCount: 9,
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    _tabbed(index);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Color.fromARGB(255, 141, 141, 141),
+                    )),
+                    child: Center(
+                        child: Text(
+                      displayExOh[index],
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    )),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: Column(
+                children: [
+                  Text(
+                    'Tic            Tac            Toe',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: const Color.fromARGB(255, 162, 162, 162)),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        _resetGame();
+                      },
+                      child: Icon(Icons.restart_alt))
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _tabbed(int index) {
+    setState(() {
+      if (ohTurn && displayExOh[index] == '') {
+        displayExOh[index] = 'o';
+        filledBoxes += 1;
+      } else if (!ohTurn && displayExOh[index] == '') {
+        displayExOh[index] = 'x';
+        filledBoxes += 1;
+      }
+      ohTurn = !ohTurn;
+      _checkWinner();
+    });
+  }
+
+  void _checkWinner() {
+    if (displayExOh[0] == displayExOh[1] &&
+        displayExOh[0] == displayExOh[2] &&
+        displayExOh[0] != '') {
+      _showWinDialog(displayExOh[0]);
+    }
+
+    if (displayExOh[3] == displayExOh[4] &&
+        displayExOh[3] == displayExOh[5] &&
+        displayExOh[3] != '') {
+      _showWinDialog(displayExOh[3]);
+    }
+
+    if (displayExOh[6] == displayExOh[7] &&
+        displayExOh[6] == displayExOh[8] &&
+        displayExOh[6] != '') {
+      _showWinDialog(displayExOh[6]);
+    }
+
+    if (displayExOh[0] == displayExOh[3] &&
+        displayExOh[0] == displayExOh[6] &&
+        displayExOh[0] != '') {
+      _showWinDialog(displayExOh[0]);
+    }
+
+    if (displayExOh[1] == displayExOh[4] &&
+        displayExOh[1] == displayExOh[7] &&
+        displayExOh[1] != '') {
+      _showWinDialog(displayExOh[1]);
+    }
+
+    if (displayExOh[2] == displayExOh[5] &&
+        displayExOh[2] == displayExOh[8] &&
+        displayExOh[2] != '') {
+      _showWinDialog(displayExOh[2]);
+    }
+
+    if (displayExOh[0] == displayExOh[4] &&
+        displayExOh[0] == displayExOh[8] &&
+        displayExOh[0] != '') {
+      _showWinDialog(displayExOh[0]);
+    }
+
+    if (displayExOh[2] == displayExOh[4] &&
+        displayExOh[2] == displayExOh[6] &&
+        displayExOh[2] != '') {
+      _showWinDialog(displayExOh[2]);
+    } else if (filledBoxes == 9) {
+      _showDrawDialog();
+    }
+  }
+
+  void _showDrawDialog() {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Draw'),
+          actions: [
+            ElevatedButton(
+              child: Text('Play again'),
+              onPressed: () {
+                _clearBoard();
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  void _showWinDialog(String winner) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('WINNER IS $winner'),
+          actions: [
+            ElevatedButton(
+              child: Text('Play again'),
+              onPressed: () {
+                _clearBoard();
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+    if (winner == 'x') {
+      exScore += 1;
+    } else if (winner == 'o') {
+      ohSecore += 1;
+    }
+  }
+
+  void _clearBoard() {
+    setState(() {
+      for (int i = 0; i < 9; i++) {
+        displayExOh[i] = '';
+      }
+    });
+    filledBoxes = 0;
+  }
+
+  void _resetGame() {
+    setState(() {
+      exScore = 0;
+      ohSecore = 0;
+      for (int i = 0; i < 9; i++) {
+        displayExOh[i] = '';
+      }
+      filledBoxes = 0;
+      ohTurn = true;
+    });
+  }
+}
